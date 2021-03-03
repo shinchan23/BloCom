10062009b4b03e77684ac06c4e4f3e2e34958887
Rajat Misra <rajatmisra23@gmail.com>
2021-03-03 18:43:19 +0530

    BLCO-4

---
+++ BloCom/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloCom.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String UserName { get; set; }
+        public String Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
